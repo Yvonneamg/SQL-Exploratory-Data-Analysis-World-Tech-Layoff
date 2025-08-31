067791c63d090c0397b37aac8c620811e2895158
Yvonne Akinyi Amugaga <amugagayvonne@gmail.com>
2025-08-31 14:34:18 +0200

    Update SQL-Exploratory-Data-Analysis-World-Tech-Layoff.sql

--- SQL-Exploratory-Data-Analysis-World-Tech-Layoff.sql
+++ SQL-Exploratory-Data-Analysis-World-Tech-Layoff.sql
@@ -34,6 +34,14 @@ from layoff_staging2
 group by company
 order by total_laidoff Desc;
 
+-- Determine the total laid off by company in USA only
+select company,
+sum(total_laid_off) total_laidoff
+from layoff_staging2
+where country='United States'
+group by company
+order by total_laidoff Desc;
+
 -- Determine the Companies with zero layoffs
 select company,
 sum(total_laid_off) total_laidoff
@@ -41,3 +49,38 @@ from layoff_staging2
 where total_laid_off=0
 group by company
 order by total_laidoff Desc;
+
+-- Select top 5 companies by fundsraised
+select Country,
+CITY,
+company,
+sum(total_laid_off) as Total_laid_off,
+sum(Fundsraised2) as Total_funds_raised
+from layoff_staging2
+group by country,CITY,company
+order by Total_funds_raised desc limit 5;
+
+-- Select top 5 companies by fundsraised in USA only
+select Country,
+CITY,
+company,
+sum(total_laid_off) as Total_laid_off,
+sum(Fundsraised2) as Total_funds_raised
+from layoff_staging2
+where country='United States'
+group by country,CITY,company
+order by Total_funds_raised desc limit 5;
+
+-- Select top 5 industry by Total laid off 
+select country,
+CITY,
+company, 
+industry,
+sum(total_laid_off) as Total_Laid_Off,
+sum(Fundsraised2) as Total_Fundsraised
+from layoff_staging2
+group by country,CITY,company,industry
+order by Total_Laid_Off desc;
+
+select * from layoff_staging2;
+
